e758729ba4057f78ede319b3bcff86ed70b0093b
Asier <blazgar@gmail.com>
2020-02-12 14:25:33 +0100

    mail

--- ProyectoHADS/Registro.aspx.vb
+++ ProyectoHADS/Registro.aspx.vb
@@ -16,4 +16,24 @@
     Protected Sub Button1_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Protected Sub Button1_Click1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim EnviarMail As New EnviarMail.EnviarMail
+
+
+
+
+
+        ' Dim numAleatorio As New Random()
+        ' Dim valorAleatorio As Integer = numAleatorio.Next(10000, 99999)
+
+        'EnviarMail.enviarEmail(emailtext.Text, valorAleatorio)
+        ' MsgBox(valorAleatorio)
+
+        EnviarMail.enviarEmail(emailtext.Text)
+
+
+
+
+    End Sub
 End Class
