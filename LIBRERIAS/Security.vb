fc3a628370f81c85137144c0eac6e5d3196abe53
Asier <blazgar@gmail.com>
2020-03-31 20:15:56 +0200

    Create Security.vb

---
+++ LIBRERIAS/Security.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Security
+
+    Shared Function getMd5Hash(ByVal md5Hash As MD5, ByVal input As String) As String
+        ' Create a new instance of the MD5 object.
+        Dim md5Hasher As MD5 = MD5.Create()
+
+        ' Convert the input string to a byte array and compute the hash.
+        Dim data As Byte() = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input))
+
+        ' Create a new Stringbuilder to collect the bytes
+        ' and create a string.
+        Dim sBuilder As New StringBuilder()
+
+        ' Loop through each byte of the hashed data
+        ' and format each one as a hexadecimal string.
+        Dim i As Integer
+        For i = 0 To data.Length - 1
+            sBuilder.Append(data(i).ToString("x2"))
+        Next i
+
+        ' Return the hexadecimal string.
+        Return sBuilder.ToString()
+
+    End Function
+
+
+End Class
